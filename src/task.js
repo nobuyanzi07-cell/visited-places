0e4c28ad3673208b695a64c9b2ad48ab3844ffb3
Nathan Oketch <nobuyanzi07@gmail.com>
2026-05-12 20:48:10 +0300

    feat(task.js): Task js completion

--- src/task.js
+++ src/task.js
@@ -1,9 +1,23 @@
 function Task(description) {
     this.description = description;
     this.done = false;
+    console.log(`Task created: ${this.description} (done: ${this.done})`);
 }
 
+Task.prototype.markDone = function() {
+    this.done = true;
+    console.log(`Task marked done: ${this.description}`);
+};
 
+Task.prototype.remove = function(tasks) {
+    console.log(`Removing task: ${this.description} from list`);
+}
+
+const updateedTask = task.filter(t=>t !== this);
+console.log(`Remaining tasks: ${updateedTask.map(t => t.description).join(', ') || 'No tasks left'}`);
+return updateedTask;
+
+module.exports = Task;
     
 
 
