cf409eb34f41439fa5a94f12893ee451c5d9db83
Nathan Oketch <nobuyanzi07@gmail.com>
2026-05-13 09:30:31 +0300

    feat(app.js): Import constructor done

---
+++ app.js
@@ -0,0 +1,46 @@
+// Import Constructors
+function Contact(name, Phone, email) {
+    this.name = name;
+    this.phone = Phone;
+    this.email = email;
+    console.log(`Created contact: ${this.display()}`);
+}
+
+Contact.prototype.updatePhone = function(newPhone) {
+    console.log(`Displaying contact: ${this.name}`);
+    return `${this.name} - ${this.phone} - ${this.email}`;
+};
+
+function AddressBook() {
+    this.contacts = [];
+    console.log('AddressBook creation');
+}
+
+AddressBook.prototype.addContact = function(contact) {
+    this.contacts.push(contact);
+    console.log(`Contact added: ${contact.display()}`);
+};
+
+function Place(location, landmark) {
+    this.location = location;
+    this.landmark = landmark;
+    console.log(`Place created: ${this.location}, ${this.landmark}`);
+}
+
+Place.prototype.summary = function() {
+    console.log(`Summarizing place: ${this.location}`);
+    return `${this.location} is located in ${this.landmark}`;
+};
+
+function Task(description) {
+    this.description = description;
+    this.done = false;
+    console.log(`Task creation: ${this.description}`);
+}
+
+Task.prototype.markDone = function() {
+    this.done = true;
+    console.log(`Task marked done: ${this.description}`);
+};
+
+
--- tests/place.test.js
+++ tests/place.test.js
@@ -1,6 +1,6 @@
 const Place  = require('../src/place');
 
-Test('summaries place', () => {
+test('summaries place', () => {
     const p = new Place('Uhuru Park', 'Nairobi County');
     expect(p.summary()).toBe('Uhuru Park is located in Nairobi County');
 });
