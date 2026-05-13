fff1b36e985a546958b9f78eff8f964aca413b44
Nathan Oketch <nobuyanzi07@gmail.com>
2026-05-13 08:29:19 +0300

    feat(place.test.js): Creation of place test file

---
+++ tests/place.test.js
@@ -0,0 +1,6 @@
+const Place  = require('../src/place');
+
+Test('summaries place', () => {
+    const p = new Place('Uhuru Park', 'Nairobi County');
+    expect(p.summary()).toBe('Uhuru Park is located in Nairobi County');
+});
