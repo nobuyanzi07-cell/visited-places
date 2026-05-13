2c2affdf601192eb40d6f452dd075b702d676eeb
Nathan Oketch <nobuyanzi07@gmail.com>
2026-05-13 08:35:39 +0300

    feat(contact.test.js): Creation of contacttest file

---
+++ tests/contact.test.js
@@ -0,0 +1,7 @@
+const Contact = require('../src/Contact');
+
+Test('updates phone number', () => {
+    const c = new Contact('Obuyanzi Nathan', '0723242452', 'nobuya67@example.com');
+    c.updatePhone('0712345678');
+    expect(c.phone).toBe('0712345678');
+});
