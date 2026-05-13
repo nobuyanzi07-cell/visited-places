2b97a5b9c33f67f97b03e77d875d6617eb35dce3
Nathan Oketch <nobuyanzi07@gmail.com>
2026-05-13 08:50:48 +0300

    Feat(addressBook.test.js): Creation of Adress book tester

---
+++ tests/addressbook.test.js
@@ -0,0 +1,16 @@
+const AddressBook = require('../src/AddressBook');
+
+Test('adds and finds a contact', () => {
+    const book = new AddressBook();
+    const c = new Contact('Obuyanzi Nathan', '0723242452', 'nobuya67@example.com');
+    book.addContact(c);
+    expect(book.findContact('Obuyanzi Nathan')).toBe(c);
+});
+
+test('removes a contact', () => {
+    const book = new AddressBook();
+    const c = new Contact('Obuyanzi Nathan', '0723242452', 'nobuya67@example.com');
+    book.addContact(c);
+    book.removeContact('Obuyanzi Nathan');
+    expect(book.contact.length).toBe(0);
+});
