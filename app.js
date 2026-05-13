c2c15acb71bfd774f7cfd70979f7e5aaa0b19253
Nathan Oketch <nobuyanzi07@gmail.com>
2026-05-13 12:48:35 +0300

    chore(app.js): clearing all nessecary mistakes in app.js file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Import Constructors
+// contact
 function Contact(name, Phone, email) {
     this.name = name;
     this.phone = Phone;
@@ -11,16 +11,18 @@ Contact.prototype.updatePhone = function(newPhone) {
     return `${this.name} - ${this.phone} - ${this.email}`;
 };
 
+//AddressBook
 function AddressBook() {
     this.contacts = [];
-    console.log('AddressBook creation');
+    console.log('AddressBook initialized');
 }
 
 AddressBook.prototype.addContact = function(contact) {
     this.contacts.push(contact);
-    console.log(`Contact added: ${contact.display()}`);
+    console.log(`Contact added to AddressBook: ${contact.name}`);;
 };
 
+//place
 function Place(location, landmark) {
     this.location = location;
     this.landmark = landmark;
@@ -32,10 +34,11 @@ Place.prototype.summary = function() {
     return `${this.location} is located in ${this.landmark}`;
 };
 
+//task
 function Task(description) {
     this.description = description;
     this.done = false;
-    console.log(`Task creation: ${this.description}`);
+    console.log(`Task created: ${this.description}`);
 }
 
 Task.prototype.markDone = function() {
@@ -43,40 +46,41 @@ Task.prototype.markDone = function() {
     console.log(`Task marked done: ${this.description}`);
 };
 
-//Instantiate managers
+// managers
 const book = new AddressBook();
 const places = [];
 const tasks = [];
 
 //Address book interface
-document.getElementById('addContact').addEventListener('click', () => {
-    const name = document.getElementById('contactName').value;
-    const phone = document.getElementById('contactPhone').value;
-    const email = document.getElementById('contactEmail').value;
+document.getElementById('add-contact').addEventListener('click', () => {
+    const name = document.getElementById('contact-name').value;
+    const phone = document.getElementById('contact-phone').value;
+    const email = document.getElementById('contact-email').value;
     const contact = new Contact(name, phone, email);
     book.addContact(contact);
     const li = document.createElement('li');
     li.textContent = contact.display();
-    document.getElementById('contactList').appendChild(li);
+    document.getElementById('contact-list').appendChild(li);
+    console.log(`Contact added to list`);
 });
 
 //Place Interface
-document.getElementById('addPlace').addEventListener('click', () => {
-    const location = document.getElementById('placeLocation').value;
-    const landmark = document.getElementById('placeLandmark').value;
+document.getElementById('add-place').addEventListener('click', () => {
+    const location = document.getElementById('place-location').value;
+    const landmark = document.getElementById('place-landmark').value;
     const place = new Place(location, landmark);
     places.push(place);
 
 
     const li = document.createElement('li');
     li.textContent = place.summary();
-    document.getElementById('placeList').appendChild(li);
+    document.getElementById('place-list').appendChild(li);
     console.log(`Place added to list`);
 });
 
 //To do interface
-document.getElementById('addTask').addEventListener('click', () => {
-    const desc = document.getElementById('taskDesc').value;
+document.getElementById('add-task').addEventListener('click', () => {
+    const desc = document.getElementById('task-desc').value;
     const task = new Task(desc);
     tasks.push(task);
 
@@ -96,10 +100,9 @@ document.getElementById('addTask').addEventListener('click', () => {
         li.remove();
         console.log(`Task removed: ${task.description}`);
     };
-
     li.appendChild(doneBtn);
     li.appendChild(removeBtn);
-    document.getElementById('taskList').appendChild(li);
+    document.getElementById('task-list').appendChild(li);
     console.log(`Task added to list`);
 });
     
